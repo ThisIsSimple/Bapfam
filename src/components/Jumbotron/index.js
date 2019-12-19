78f50570e1e6f08eff8617ffd8df4c62025d9fef
전윤민 <thisissipmle2731@gmail.com>
2019-12-20 01:00:16 +0900

    Jumbotron fixed

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import * as firebase from "firebase";
 
 const Jumbotron = () => {
 	return (
 		<div className="main-jumbotron jumbotron jumbotron-fluid bg-light-purple d-flex justify-content-center align-items-center mb-0">
 			<div className="container">
 				<p className="lead mb-0">안녕하세요,</p>
-				<h1 className="display-5 font-weight-bold">윤민님</h1>
+				<h1 className="display-5 font-weight-bold">{firebase.auth().currentUser.displayName}님</h1>
 			</div>
 		</div>
 	);
